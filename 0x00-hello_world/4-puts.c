d44bfc9352005285501d61b97f51ea2158242653
babayaga <shsaids207@gmail.com>
2023-10-05 15:02:09 -0700

    Preprocessor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-int main()
+/**
+ * main - this function is for puts
+ * @void:  parameter
+ * return: always 0
+ * Desciption: this is for alx
+ */
+int main(void)
 {
-   puts ("\"Programming is like building a multilingual puzzle");
-   return 0 ;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
